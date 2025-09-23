71a4afd6258656381de2358e817798465a9cd5f9
Ziar Sabir <ziar.sabir@gmail.com>
2025-09-23 12:35:02 +0100

    deployment changes

--- server.js
+++ server.js
@@ -279,4 +279,4 @@ app.get("/api/news", async (req, res) => {
 });
 
 const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => console.log(`News proxy on http://localhost:${PORT}`));
+app.listen(PORT,"0.0.0.0", () => console.log(`News proxy on ${PORT}`));
